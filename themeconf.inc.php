42de64edbb38921d9662fb76e67d2c1b1ab00378
Thomas Feuster <shopping@feuster.com>
2019-08-06 15:48:01 +0200

    Update themeconf.inc.php
    finalized update to handle recent_pics

--- themeconf.inc.php
+++ themeconf.inc.php
@@ -197,16 +197,15 @@ function elegant_slick_pano_max_size($content, $element_info)
 /*
 No category page
 */
+// TFE, 20190806: make sure our handler is called early
 add_event_handler('loc_end_index_thumbnails', 'elegant_slick_skip_cat', 55);
 function elegant_slick_skip_cat($tpl_thumbnails_var)
 {
-//	print_r('skip category');
+	//print_r('skip category');
 	global $page, $template;
 
 	// check, if we should do anything
-	if (// TFE, 20190804: check if $page or $template are set; if not we're e.g. showing recent pics...
-		empty($page) or empty($template) or 
-		// is it something else than the category page
+	if (// is it something else than the category page
 		!isset($page['category']) or
 		// is the template flag set to not suppress the category page?
 		!isset($template->get_template_vars('elegant_slick')['p_no_cat_page']) or
